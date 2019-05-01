61ed77dafc33ace6d7d33dd70aadd717771a5b50
c2d7fa <c2d7fa@gmail.com>
2019-05-01 14:31:42 +0200

    Reimplement item descriptions

---
+++ description.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(require (prefix-in data: "item-data.rkt"))
+
+(define description-key 'description)
+
+(define (register-property-description item-data)
+  (data:new-property item-data #:key description-key #:name "Description" #:default "(No description)"))
+
+(define (get-description item-data item)
+  (data:get-property item-data item description-key))
+
+(define (set-description item-data item description)
+  (data:set-property item-data item description-key description))
+
+;; Returns (values new-item-data new-item).
+(define (new-item-with-description item-data description)
+  (define-values (new-item-data new-item) (data:new-item item-data))
+  (values
+   (set-description new-item-data new-item description)
+   new-item))
+
