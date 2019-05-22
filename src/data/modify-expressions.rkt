1fcb122bd4c788202210eb6ef2ac78c59033e59c
c2d7fa <c2d7fa@gmail.com>
2019-05-22 19:15:46 +0200

    Update modify-expressions to use property-type

--- src/data/modify-expressions.rkt
+++ src/data/modify-expressions.rkt
@@ -10,26 +10,28 @@
 ;; Take a modify expression and return a function that will update the
 ;; given item in the given item-data according to the
 ;; modify-expression.
-(define ((evaluate-modify-expression modify-expression) item-data item)
+(define ((evaluate-modify-expression modify-expression #:property-types property-types) item-data item)
   (match modify-expression
     (`(,property ,operator ,literal-expr) #:when (symbol? operator)
      (if (eq? operator ':)
-         (data:set-property item-data item property (val:evaluate-literal literal-expr))
-         (data:set-property item-data item property (operators:evaluate-operator-expression
-                                                     operators:common-operators
-                                                     (list (data:get-property item-data item property)
-                                                           operator
-                                                           (val:evaluate-literal literal-expr))))))
+         (data:set-property-by-key #:property-types property-types
+                                   item-data item property (val:evaluate-literal literal-expr))
+         (data:set-property-by-key #:property-types property-types
+                                   item-data item property (operators:evaluate-operator-expression
+                                                            operators:common-operators
+                                                            (list (data:get-property-by-key item-data item property #:property-types property-types)
+                                                                  operator
+                                                                  (val:evaluate-literal literal-expr))))))
     (`(and ,subexpressions ...)
      (foldl (λ (subexpression item-data)
-              ((evaluate-modify-expression subexpression) item-data item))
+              ((evaluate-modify-expression subexpression #:property-types property-types) item-data item))
             item-data
             subexpressions))))
 
 ;; Returns new-item-data after modifying all items in item-data
 ;; according to modify-expression.
-(define (modify-items item-data items modify-expression)
+(define (modify-items item-data items modify-expression #:property-types property-types)
   (foldl (λ (item item-data)
-           ((evaluate-modify-expression modify-expression) item-data item))
+           ((evaluate-modify-expression modify-expression #:property-types property-types) item-data item))
          item-data
          (set->list items)))
