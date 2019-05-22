54a268bd825ecb5fe9c0b45b5b3d4bb3f3565dc4
c2d7fa <c2d7fa@gmail.com>
2019-05-22 18:58:58 +0200

    Reimplement sort-items-by-urgency-descending

--- src/properties/urgency.rkt
+++ src/properties/urgency.rkt
@@ -3,9 +3,21 @@
 (provide (all-defined-out))
 
 (require
+  (prefix-in item: "../data/item-data.rkt")
   (prefix-in prop: "../data/property-type.rkt")
   (prefix-in val: "../data/values.rkt"))
 
 (define base-urgency-property-type
   (prop:make-property-type #:key 'baseurgency
                            #:default (val:make-number 0)))
+
+(define (calculate-urgency item-data item)
+  (item:get-property item-data item base-urgency-property-type))
+
+;; Takes item-data and a set of items, returns sorted list of items.
+(define (sort-items-by-urgency-descending item-data item-set)
+  (define (number>= x y)
+    (>= (val:unwrap-number x) (val:unwrap-number y)))
+  (sort (set->list item-set) number>=
+        #:key (λ (item)
+                (calculate-urgency item-data item))))
