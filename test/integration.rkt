5d04e1991eab9872a85efb41f30c919661041116
c2d7fa <c2d7fa@gmail.com>
2019-06-06 19:39:25 +0200

    Add integration test: Adding and activating single context

--- test/integration.rkt
+++ test/integration.rkt
@@ -9,6 +9,21 @@
                                "add description:{item 2} tags+tag1"
                                "add description:{item 3} tags+tag2"))
            (items (run-list-command "tags+tag2" state)))
-      (writeln items)
       (check-equal? (get-descriptions items state)
-                    (set "item 1" "item 3")))))
+                    (set "item 1" "item 3"))))
+
+  (test-case "adding and activating single context"
+    (let* ((state (build-state "add description:{item 1} status:inactive"
+                               "add description:{item 2}"
+                               "add description:{item 3} status:done"
+                               "add description:{item 4} status:inactive tags+tagged"
+                               "add description:{item 5} status:done"
+                               "context add unfinished filter !status:done"
+                               "@unfinished"))
+           (all-items (run-list-command "" state))
+           (untagged-items (run-list-command "tags-tagged" state)))
+      (check-equal? (get-descriptions all-items state)
+                    (set "item 1" "item 2" "item 4"))
+      (check-equal? (get-descriptions untagged-items state)
+                    (set "item 1" "item 2"))))
+  )
