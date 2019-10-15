0ec164060ed3f8773ad812ee1501a2137b05e6c6
c2d7fa <c2d7fa@gmail.com>
2019-10-15 20:35:53 +0200

    Improve file format
    This style should be more extensible. The top-level structure is not a key-value
    map rather than a plain list, and it contains a file-verion field which can be
    used to ensure backward compatibility in the future.
    
    This commit also cleans up the code for serialization slightly and reduces
    coupling between this code and other parts of the program.

--- src/serialize.rkt
+++ src/serialize.rkt
@@ -1,169 +1,137 @@
 #lang racket
 
-(provide serialize-state
-         deserialize-state
-         save-state-to-file
+(provide save-state-to-string
+         load-state-from-string
          save-state
          load-state)
 
-;; TODO: This module currently depends on implementation details of basically
-;; all modules. This is probably a bad idea. I should think about how to improve
-;; this situation.
+(define file-version (make-parameter 0))
 
-(require (prefix-in val: "untask/core/value.rkt")
-         (prefix-in dt: "datetime.rkt"))
+(define (serialize-value x)
+  (cond
+    ((val:string-value? x) `(string ,(val:unwrap-string x)))
+    ((val:number-value? x) `(number ,(val:unwrap-number x)))
+    ((val:item-value? x) `(item ,(val:unwrap-item x)))
+    ((val:boolean-value? x) `(boolean ,(val:unwrap-boolean x)))
+    ((val:date-value? x) (let ((d (val:unwrap-date x)))
+                           `(date (,(dt:datetime-year d)
+                                   ,(dt:datetime-month d)
+                                   ,(dt:datetime-day d)
+                                   ,(dt:datetime-hour d)
+                                   ,(dt:datetime-minute d)))))
+    ((val:set-value? x) `(set ,(map serialize-value (set->list (val:unwrap-set x)))))
+    ((val:empty-value? x) #f)
+    (else (error (format "Unable to serialize value: ~S" x)))))
 
-;; In order to be able to easily write and read to files in a consistent way, we
-;; want the values that we read/write to be simple S-expressions which may
-;; contains symbols, numbers and strings.
-;;
-;; Creating such an S-expression from a more complex representation we call
-;; "preparing", and we call the opposite process "unpreparing".
-
-(define (prepare-value v)
-  (match v
-    ;; TODO: Dependency on value module
-    (`(string . ,s) `(string ,s))
-    (`(number . ,n) `(number ,n))
-    (`(item . ,i) `(item ,i))
-    (`(boolean . ,b) `(boolean ,b))
-    (`(date . ,d) `(date ,d))       ; TODO: Dependency on datetime module; representation could change
-    (`(set . ,s) `(set ,@(map prepare-value (set->list s))))
-    (#f #f)
-    (else (error (format "Unknown value type for ~S" v)))))
-
-(define (unprepare-value p)
-  (match p
+(define (deserialize-value x)
+  (match x
     (`(string ,s) (val:make-string s))
     (`(number ,n) (val:make-number n))
     (`(item ,i) (val:make-item i))
     (`(boolean ,b) (val:make-boolean b))
     (`(date (,year ,month ,day ,hour ,minute)) (val:make-date (dt:datetime year month day hour minute)))
-    (`(set ,@s) (val:make-set (apply set (map unprepare-value s))))
+    (`(set ,s) (val:make-set (apply set (map deserialize-value s))))
     (#f #f)
-    (else (error (format "Unknown value type for ~S" p)))))
+    (else (error (format "Unable to deserialize value: ~S" x)))))
 
-(define (prepare-hash h #:prepare-k prepare-k #:prepare-v prepare-v)
-  (map (λ (k) (list (prepare-k k) (prepare-v (hash-ref h k))))
+(define (serialize-hash h #:serialize-key (serialize-key identity) #:serialize-value (serialize-value identity))
+  (map (λ (k) (list (serialize-key k) (serialize-value (hash-ref h k))))
        (hash-keys h)))
 
-(define (unprepare-hash kvs #:unprepare-k unprepare-k #:unprepare-v unprepare-v)
+(define (deserialize-hash kvs #:deserialize-key (deserialize-key identity) #:deserialize-value (deserialize-value identity))
   (apply hash (apply append (map (λ (kv)
-                                   (list (unprepare-k (car kv))
-                                         (unprepare-v (cadr kv))))
+                                   (list (deserialize-key (car kv))
+                                         (deserialize-value (cadr kv))))
                                  kvs))))
 
-(require (prefix-in export: "./untask/core/export.rkt"))
+(define (serialize-active-contexts active-contexts) (set->list active-contexts))
+(define (deserialize-active-contexts active-contexts) (list->set active-contexts))
 
-(define st (export:read-state-from-file "../tasks.t"))
-
-(require (prefix-in a: "attribute.rkt")
-         (prefix-in state: "./untask/core/state.rkt")
-         (prefix-in context: "./untask/core/context.rkt")
-         (prefix-in item: "./untask/core/item.rkt"))
-
-;; TODO: Dependencies on state (probably OK) and context (probably not OK)
-(define (prepare-active-contexts active-contexts)
-  (set->list active-contexts))
-(define (unprepare-active-contexts active-contexts)
-  (list->set active-contexts))
-
-;; TODO: This depends on the representations of literals.
-(define (prepare-literal l)
-  (match l
+;; NOTE: This depends on the representations of literals.
+(define (serialize-literal x)
+  (match x
     (`(string . ,s) `(string ,s))
     (`(number . ,n) `(number ,n))
     (`(item . ,i) `(item ,i))
     (`(boolean . ,b) `(boolean ,b))
-    (`(set . ,s) `(set . ,(map prepare-literal s)))
-    (`(date . ,d) `(date . ,d))))
-(define (unprepare-literal l)
-  (match l
+    (`(set . ,s) `(set . ,(map serialize-literal s)))
+    (`(date . ,d) `(date . ,d))
+    (else (error (format "Could not serialize literal: ~S" x)))))
+(define (deserialize-literal x)
+  (match x
     (`(string ,s) `(string . ,s))
     (`(number ,n) `(number . ,n))
     (`(item ,i) `(item . ,i))
     (`(boolean ,b) `(boolean . ,b))
-    (`(set . ,s) `(set . ,(map unprepare-literal s)))
-    (`(date . ,d) `(date . ,d))))
+    (`(set . ,s) `(set . ,(map deserialize-literal s)))
+    (`(date . ,d) `(date . ,d))
+    (else (error (format "Could not deserialize literal: ~S" x)))))
 
-;; TODO: This depends on the representation of filter expressions and modify
-;; expressions. Consider if that is OK.
-(define (prepare-fm-expression fme)
+;; NOTE: This depends on the representation of filer expressions and modify
+;; expressions.
+(define (serialize-fm-expression fme)
   (match fme
-    (`(and ,subexprs ...) `(and ,@(map prepare-fm-expression subexprs)))
-    (`(or ,subexprs ...) `(or ,@(map prepare-fm-expression subexprs)))
-    (`(not ,subexpr) `(not ,(prepare-fm-expression subexpr)))
+    (`(and ,subexprs ...) `(and ,@(map serialize-fm-expression subexprs)))
+    (`(or ,subexprs ...) `(or ,@(map serialize-fm-expression subexprs)))
+    (`(not ,subexpr) `(not ,(serialize-fm-expression subexpr)))
     (`(,property ,operator ,literal-expr) #:when (symbol? operator)
-     `(,property ,operator ,(prepare-literal literal-expr)))
+     `(,property ,operator ,(serialize-literal literal-expr)))
     (`(item . ,id) `(item ,id))
-    ('() '())))
-(define (unprepare-fm-expression fme)
+    ('() '())
+    (else (error (format "Could not serialize filter/modify expression: ~S" fme)))))
+(define (deserialize-fm-expression fme)
   (match fme
-    (`(and ,subexprs ...) `(and ,@(map unprepare-fm-expression subexprs)))
-    (`(or ,subexprs ...) `(or ,@(map unprepare-fm-expression subexprs)))
-    (`(not ,subexpr) `(not ,(unprepare-fm-expression subexpr)))
+    (`(and ,subexprs ...) `(and ,@(map deserialize-fm-expression subexprs)))
+    (`(or ,subexprs ...) `(or ,@(map deserialize-fm-expression subexprs)))
+    (`(not ,subexpr) `(not ,(deserialize-fm-expression subexpr)))
     (`(,property ,operator ,literal-expr) #:when (symbol? operator)
-     `(,property ,operator ,(unprepare-literal literal-expr)))
+     `(,property ,operator ,(deserialize-literal literal-expr)))
     (`(item ,id) `(item . ,id))
-    ('() '())))
+    ('() '())
+    (else (error (format "Could not deserialize filter/modify expression: ~S" fme)))))
 
+(define (serialize-defined-contexts defined-contexts)
+  (define (serialize-context x)
+    (list (serialize-fm-expression (a:get (x context:context.filter)))
+          (serialize-fm-expression (a:get (x context:context.modify)))))
+  (serialize-hash defined-contexts #:serialize-value serialize-context))
+(define (deserialize-defined-contexts defined-contexts)
+  (define (deserialize-context x)
+    (context:context #:filter (deserialize-fm-expression (car x))
+                     #:modify (deserialize-fm-expression (cadr x))))
+  (deserialize-hash defined-contexts #:deserialize-value deserialize-context))
 
-;; TODO: Dependencies on context
-(define (prepare-defined-contexts defined-contexts)
-  (prepare-hash defined-contexts
-                #:prepare-k identity
-                #:prepare-v (λ (context)
-                              (list (prepare-fm-expression (a:get (context context:context.filter)))
-                                    (prepare-fm-expression (a:get (context context:context.modify)))))))
-(define (unprepare-defined-contexts defined-contexts)
-  (unprepare-hash defined-contexts
-                  #:unprepare-k identity
-                  #:unprepare-v (λ (context)
-                                  (context:context #:filter (unprepare-fm-expression (car context))
-                                                   #:modify (unprepare-fm-expression (cadr context))))))
-
-(define (prepare-item-data-properties properties)
-  (prepare-hash properties
-                #:prepare-k identity
-                #:prepare-v (λ (props)
-                              (prepare-hash props
-                                            #:prepare-k identity
-                                            #:prepare-v prepare-value))))
-(define (unprepare-item-data-properties properties)
-  (unprepare-hash properties
-                  #:unprepare-k identity
-                  #:unprepare-v (λ (props)
-                                  (unprepare-hash props
-                                                  #:unprepare-k identity
-                                                  #:unprepare-v unprepare-value))))
-
-(define (prepare-state st)
-  (list (prepare-defined-contexts (a:get (st state:state.defined-contexts context:contexts.definitions)))
-        (prepare-active-contexts (a:get (st state:state.active-contexts)))
-        (a:get (st state:state.item-data item:item-data.next))
-        (prepare-item-data-properties (a:get (st state:state.item-data item:item-data.properties)))))
-(define (unprepare-state st open-file)
-  (match st
-    ((list defined-contexts active-contexts next-id item-data-properties)
-     (state:state #:defined-contexts (context:contexts #:definitions (unprepare-defined-contexts defined-contexts))
-                  #:active-contexts (unprepare-active-contexts active-contexts)
-                  #:open-file open-file
-                  #:item-data (item:item-data #:next next-id
-                                              #:properties (unprepare-item-data-properties item-data-properties))))))
+(define (serialize-item-data-properties properties)
+  (serialize-hash properties #:serialize-value (λ (h) (serialize-hash h #:serialize-value serialize-value))))
+(define (deserialize-item-data-properties properties)
+  (deserialize-hash properties #:deserialize-value (λ (h) (deserialize-hash h #:deserialize-value deserialize-value))))
 
 (define (serialize-state st)
-  (~s (prepare-state st)))
-
+  (serialize-hash (hash 'file-version (file-version)
+                        'all-contexts (serialize-defined-contexts (a:get (st state:state.defined-contexts context:contexts.definitions)))
+                        'active-contexts (serialize-active-contexts (a:get (st state:state.active-contexts)))
+                        'next-item-id (a:get (st state:state.item-data item:item-data.next))
+                        'item-property-data (serialize-item-data-properties (a:get (st state:state.item-data item:item-data.properties))))))
 (define (deserialize-state st #:open-file open-file)
-  (unprepare-state (read (open-input-string st)) open-file))
+  (define h (deserialize-hash st))
+  (when (not (= (hash-ref h 'file-version) (file-version)))
+    (error (format "This version of Untask can only read file format version ~A, but the given file has version ~A." (file-version) (hash-ref h 'file-version))))
+  (state:state #:defined-contexts (context:contexts #:definitions (deserialize-defined-contexts (hash-ref h 'all-contexts)))
+               #:active-contexts (deserialize-active-contexts (hash-ref h 'active-contexts))
+               #:open-file open-file
+               #:item-data (item:item-data #:next (hash-ref h 'next-item-id)
+                                           #:properties (deserialize-item-data-properties (hash-ref h 'item-property-data)))))
 
-(define (save-state-to-file st path)
-  (with-output-to-file "test.t" #:exists 'replace
-    (thunk (displayln (serialize-state st)))))
+(define (save-state-to-string st)
+  (~s (serialize-state st)))
+
+(define (load-state-from-string st #:open-file open-file)
+  (deserialize-state (read (open-input-string st)) open-file))
 
 (define (save-state st)
-  (save-state-to-file st (a:get (st state:state.open-file))))
+  (with-output-to-file (a:get (st state:state.open-file)) #:exists 'replace
+    (thunk (displayln (save-state-to-string st)))))
 
 (define (load-state path)
-  (deserialize-state (port->string (open-input-file path) #:close? #t)
+  (deserialize-state (read (open-input-file path))
                      #:open-file path))
