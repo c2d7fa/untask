d1e239a1f052d24b163fc9b2c29bfc735b902abc
c2d7fa <c2d7fa@gmail.com>
2019-10-18 14:38:49 +0200

    Update tests to work with refactor

--- test/test-serialization.rkt
+++ test/test-serialization.rkt
@@ -5,7 +5,7 @@
          "../src/untask/core/serialize.rkt"
 
          "../src/untask/core/state.rkt"
-         (prefix-in item: "../src/untask/core/item.rkt")
+         (prefix-in p: "../src/untask/core/property.rkt")
          "../src/untask/properties/description.rkt"
          "../src/untask/properties/dependencies.rkt"
          (prefix-in a: "../src/attribute.rkt")
@@ -18,10 +18,10 @@
    "Serialization"
    (test-case "Loading file produces some correct item data"
      (define st (load-state "./example.t"))
-     (define item-data (a:get (st state.item-data)))
-     (check-equal? (item:get-property item-data 1 description-property-type) (val:make-string "Task 1"))
-     (check-equal? (item:get-property item-data 2 description-property-type) (val:make-string "Task 2"))
-     (check-equal? (item:get-property item-data 3 blocks-property-type) (val:make-set (set (val:make-item 2)))))
+     (define item-state (a:get-path (st state.item-state)))
+     (check-equal? (p:get item-state 1 description-property) (val:make-string "Task 1"))
+     (check-equal? (p:get item-state 2 description-property) (val:make-string "Task 2"))
+     (check-equal? (p:get item-state 3 blocks-property) (val:make-set (set (val:make-item 2)))))
    (test-case "Loading saved data gives original result"
      (define st (load-state "./example.t"))
      (check-equal? st (load-state-from-string #:open-file "./example.t" (save-state-to-string st))))
