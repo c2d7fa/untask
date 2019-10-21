c3455d8ba148235d894d2b08341c9f0360310369
c2d7fa <c2d7fa@gmail.com>
2019-10-21 15:56:31 +0200

    Add documentation for copy-recur procedure

--- src/untask/properties/date.rkt
+++ src/untask/properties/date.rkt
@@ -2,7 +2,8 @@
 
 (provide wait-property
          date-property
-         wait-active?)
+         wait-active?
+         copy-recur)
 
 (require
  (prefix-in p: "../core/property.rkt")
@@ -21,3 +22,19 @@
   (let ((wait (p:get item-state item wait-property)))
     (or (not wait)
         (not (dt:future? (val:unwrap-date wait))))))
+
+;; Make copies of a task corresponding to recurrences of that task. The copies
+;; will have their date and wait properties set according to the given
+;; parameters.
+;;
+;; The first task will be created on the date given by 'start' and no tasks will
+;; be created after 'end'. Each task will be separated by 'skip' (which is an
+;; integer) days.
+;;
+;; The 'wait' property will be determined by the 'date' property such that the
+;; time between the two is constant. If the item being copies does not have any
+;; 'wait', then the resulting items also won't have any 'wait'.
+;;
+;; If the source item does not exist, do nothing.
+(define (copy-recur item-state item #:start start #:end end #:skip skip)
+  #f)
