f19f2570af60c674bec3370eac59ae161b0ff955
c2d7fa <c2d7fa@gmail.com>
2019-10-21 19:13:43 +0200

    Implement copy-recur

--- src/untask/properties/date.rkt
+++ src/untask/properties/date.rkt
@@ -6,9 +6,11 @@
          copy-recur)
 
 (require
+ (prefix-in i: untask/src/untask/core/item)
  (prefix-in p: untask/src/untask/core/property)
  (prefix-in val:  untask/src/untask/core/value)
- (prefix-in dt: untask/src/datetime))
+ (prefix-in dt: untask/src/datetime)
+ untask/src/squiggle)
 
 (define wait-property
   (p:property #:name 'wait
@@ -38,4 +40,24 @@
 ;;
 ;; If the source item does not exist, do nothing.
 (define (copy-recur item-state item #:start start #:end end #:skip skip)
-  (values item-state (list)))
+  (define wait-offset
+    (let ((w (p:get item-state item wait-property))
+          (d (p:get item-state item date-property)))
+      (if (and w d)
+          (dt:days-between (val:unwrap-date w) (val:unwrap-date d))
+          #f)))
+  (let ((item-state/new-items
+         (foldl (λ (d item-state/new-items)
+                  (let ((item-state (car item-state/new-items))
+                        (new-items  (cdr item-state/new-items)))
+                    (let-values (((item-state* item*) (i:clone item-state item)))
+                      (~>! item-state*
+                           (p:set item* date-property (val:make-date d)))
+                      (when wait-offset
+                        (~>! item-state*
+                             (p:set item* wait-property (val:make-date (dt:add-days d wait-offset)))))
+                      (cons item-state* (append new-items (list item*))))))
+                (cons item-state (list))
+                (dt:date-range start end skip))))
+    (values (car item-state/new-items)
+            (cdr item-state/new-items))))
