51578e710246f4f318088c6d7e43f9230f452e04
c2d7fa <c2d7fa@gmail.com>
2019-11-01 14:25:57 +0100

    Implement execution of graphical agenda command
    The user/graphical-agenda module is extremely hacky, and we still need to
    actually implement the "effort" property.

--- src/untask/user/graphical-agenda.rkt
+++ src/untask/user/graphical-agenda.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
-(require untask/src/untask/core/serialize
+(provide display-graphical-agenda!)
+
+(require racket/gui
+         untask/src/untask/core/serialize
          untask/src/untask/core/state
          (prefix-in v: untask/src/untask/core/value)
          (prefix-in p: untask/src/untask/core/property)
@@ -10,109 +13,93 @@
          (prefix-in a: untask/src/attribute)
          untask/src/squiggle)
 
-;; Example data
+(define (display-graphical-agenda! item-state agenda-view)
+  (define frame (new frame% (label "Agenda") (width 1200) (height 800)))
+  (define canvas (new canvas% (parent frame) (style '(no-autoclear vscroll hscroll)) (paint-callback (λ (_1 _2) (rerender! dc)))))
+  (define dc (send canvas get-dc))
 
-(define example (a:update-path ((load-state "../../../../../msc/tasks/tasks.t") state.context-state)
-                               (λ (cst) (ctx:toggle cst '((reset) (on "weekly"))))))
+  (define (truncate-text text)
+    (if (> (string-length text) 20)
+        (string-append (substring text 0 20) "...")
+        text))
 
-;; Cache data to improve rerendering performance
+  (define (translate-color color-name)
+    (case color-name
+      ((#f) (make-color #xC0 #xC0 #xC0))
+      ((red) (make-color #xE0 #x90 #x90))
+      ((green) (make-color #x90 #xE0 #x90))
+      ((yellow) (make-color #xC8 #xB0 #x90))
+      ((blue) (make-color #x80 #xA0 #xE0))
+      ((magenta) (make-color #xB0 #x80 #xC0))
+      ((cyan) (make-color #x70 #xB8 #xC0))))
 
-(define agenda-view (cmd:agenda example #:filter '()))
+  (define task-description-text-height 18)
+  (define task-description-font (make-font #:size task-description-text-height))
+  (define task-box-width 300)
+  (define padding 8)
+  (define line-height (let-values (((_1 line-height _2 _3) (begin (send dc set-font task-description-font)
+                                                                  (send dc get-text-extent ""))))
+                        line-height))
 
-;; GUI setup
+  (define (task-box-height-for-effort effort)
+    (* (+ padding line-height padding) (+ 1 effort)))
 
-(require racket/gui)
+  (define (render-task-box dc #:day day #:previous-tasks-efforts previous-tasks-efforts #:effort effort #:description description #:color color)
+    (define x (+ padding (* (+ task-box-width padding) day)))
+    (define y (foldl (λ (task-effort y)
+                       (+ y padding (task-box-height-for-effort effort)))
+                     padding
+                     previous-tasks-efforts))
+    (send dc set-pen (make-color #x00 #x00 #x00) 0 'transparent)
+    (send dc set-brush (translate-color color) 'solid)
+    (send dc draw-rounded-rectangle x y task-box-width (* (+ padding line-height padding) (+ 1 effort)) 4)
+    (send dc set-text-foreground (make-color #x30 #x30 #x30))
+    (send dc set-font task-description-font)
+    (send dc draw-text description (+ padding x) (+ padding y)))
 
-(define frame (new frame% (label "Agenda") (width 1200) (height 800)))
-(send frame show #t)
-(define canvas (new canvas% (parent frame) (style '(no-autoclear vscroll hscroll)) (paint-callback (λ (_1 _2) (rerender!)))))
-(define dc (send canvas get-dc))
+  (define temporary-hardcoded-task-effort 2) ; TODO
 
-;; Main
+  (define (render-tasks dc item-state tasks #:day day)
+    (foldl (λ (task previous-tasks-efforts)
+             (define effort temporary-hardcoded-task-effort)
+             (define description (truncate-text (v:unwrap-string (p:get item-state task (bp:ref 'description)))))
+             (define color (let ((c (p:get item-state task (bp:ref 'color))))
+                             (and c (string->symbol (v:unwrap-string c)))))
+             (render-task-box dc
+                              #:day day
+                              #:effort effort
+                              #:previous-tasks-efforts previous-tasks-efforts
+                              #:description description
+                              #:color color)
+             (append previous-tasks-efforts (list effort)))
+           (list)
+           tasks))
 
-(define (truncate-text text)
-  (if (> (string-length text) 20)
-      (string-append (substring text 0 20) "...")
-      text))
+  (define (render-agenda dc item-state agenda-view)
+    (foldl (λ (view day)
+             (render-tasks dc item-state (cdr view) #:day day)
+             (+ 1 day))
+           0
+           agenda-view))
 
-(define (translate-color color-name)
-  (case color-name
-    ((#f) (make-color #xC0 #xC0 #xC0))
-    ((red) (make-color #xE0 #x90 #x90))
-    ((green) (make-color #x90 #xE0 #x90))
-    ((yellow) (make-color #xC8 #xB0 #x90))
-    ((blue) (make-color #x80 #xA0 #xE0))
-    ((magenta) (make-color #xB0 #x80 #xC0))
-    ((cyan) (make-color #x70 #xB8 #xC0))))
+  (define (rerender! dc)
+    (send dc set-background (make-color #xE0 #xE0 #xE0))
+    (send dc clear)
+    (render-agenda dc item-state agenda-view))
 
-(define task-description-text-height 18)
-(define task-description-font (make-font #:size task-description-text-height))
-(define task-box-width 300)
-(define padding 8)
-(define line-height (let-values (((_1 line-height _2 _3) (begin (send dc set-font task-description-font)
-                                                                (send dc get-text-extent ""))))
-                      line-height))
+  (define total-width
+    (+ padding (* (+ task-box-width padding) (length agenda-view))))
 
-(define (task-box-height-for-effort effort)
-  (* (+ padding line-height padding) (+ 1 effort)))
+  (define total-height
+    (exact-ceiling
+      (apply max (map (λ (view)
+                        (foldl (λ (task height)
+                                 (+ height padding (task-box-height-for-effort temporary-hardcoded-task-effort)))
+                               padding
+                               (cdr view)))
+                      agenda-view))))
 
-(define (render-task-box dc #:day day #:previous-tasks-efforts previous-tasks-efforts #:effort effort #:description description #:color color)
-  (define x (+ padding (* (+ task-box-width padding) day)))
-  (define y (foldl (λ (task-effort y)
-                     (+ y padding (task-box-height-for-effort effort)))
-                   padding
-                   previous-tasks-efforts))
-  (send dc set-pen (make-color #x00 #x00 #x00) 0 'transparent)
-  (send dc set-brush (translate-color color) 'solid)
-  (send dc draw-rounded-rectangle x y task-box-width (* (+ padding line-height padding) (+ 1 effort)) 4)
-  (send dc set-text-foreground (make-color #x30 #x30 #x30))
-  (send dc set-font task-description-font)
-  (send dc draw-text description (+ padding x) (+ padding y)))
+  (send canvas init-auto-scrollbars total-width total-height 0 0)
+  (send frame show #t)
 
-(define temporary-hardcoded-task-effort 2) ; TODO
-
-(define (render-tasks dc item-state tasks #:day day)
-  (foldl (λ (task previous-tasks-efforts)
-           (define effort temporary-hardcoded-task-effort)
-           (define description (truncate-text (v:unwrap-string (p:get item-state task (bp:ref 'description)))))
-           (define color (let ((c (p:get item-state task (bp:ref 'color))))
-                           (and c (string->symbol (v:unwrap-string c)))))
-           (render-task-box dc
-                            #:day day
-                            #:effort effort
-                            #:previous-tasks-efforts previous-tasks-efforts
-                            #:description description
-                            #:color color)
-           (append previous-tasks-efforts (list effort)))
-         (list)
-         tasks))
-
-(define (render-agenda dc item-state agenda-view)
-  (foldl (λ (view day)
-           (render-tasks dc item-state (cdr view) #:day day)
-           (+ 1 day))
-         0
-         agenda-view))
-
-(define (rerender!)
-  (send dc set-background (make-color #xE0 #xE0 #xE0))
-  (send dc clear)
-  (render-agenda dc (a:get-path (example state.item-state)) agenda-view))
-
-;; Initialize
-
-(define total-width
-  (+ padding (* (+ task-box-width padding) (length agenda-view))))
-
-(define total-height
-  (exact-ceiling
-    (apply max (map (λ (view)
-                      (foldl (λ (task height)
-                               (+ height padding (task-box-height-for-effort temporary-hardcoded-task-effort)))
-                             padding
-                             (cdr view)))
-                    agenda-view))))
-
-(send canvas init-auto-scrollbars total-width total-height 0 0)
-
-(rerender!)
+  (yield 'wait))
