4049763d5d31e9bb42d3aeaf14cf43362d1e2537
Jonas Hvid <mail@johv.dk>
2021-12-01 13:52:57 +0100

    Add 'appimagetool' to nix-shell

--- shell.nix
+++ shell.nix
@@ -5,8 +5,16 @@ let
     rev = "98747f27ecfee70c8c97b195cbb94df80a074dda";
     sha256 = "R7jKS7A+0tZS8qD5pBr1UFcMiTdsw5bfoxgXbYsoWhM=";
   }) {};
+  appimagetool = pkgs.appimageTools.wrapType2 {
+    name = "appimagetool";
+    src = pkgs.fetchurl {
+      url = "https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage";
+      sha256 = "3zuvXKX6y+z8Lz+mcTwpq5zvqP2MHqxdKDt5yrM+Sss=";
+    };
+    extraPkgs = pkgs: [pkgs.file];
+  };
 in pkgs.mkShell {
-  buildInputs = [ pkgs.racket pkgs.earthly ];
+  buildInputs = [ pkgs.racket appimagetool pkgs.earthly ];
 }
 
 
